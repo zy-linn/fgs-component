64de0c83ab6862a86c4a42ad280a0b0473e6dca3
zy-linn <120696300+zy-linn@users.noreply.github.com>
2023-01-30 14:54:53 +0800

    Create remove.ts

---
+++ test/fixtures/remove.ts
@@ -0,0 +1,11 @@
+import ComponentDemo from '../../src/index';
+import { INPUTS } from './inputs';
+
+(async () => {
+    try {
+        const data = await new ComponentDemo().remove(INPUTS);
+        console.log(data);
+    } catch (error) {
+        console.error(error);
+    }
+})();
